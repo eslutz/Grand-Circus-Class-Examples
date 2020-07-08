a17b2c270e38722c3934b66078d59a47f1ce4502
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-07 20:20:59 -0400

    Add project files.

---
+++ Circle Radius/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Circle_Radius
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Asks the user for a the radius for a circle and then takes in the input.
+			Console.WriteLine("Enter the radius. =>");
+			string input = Console.ReadLine();
+			// Converts the input from a string to a double.
+			double radius;
+			bool validNumber = double.TryParse(input, out radius);
+			while (!validNumber)
+			{
+				Console.WriteLine("Invalid input. Please enter a number. =>");
+				input = Console.ReadLine();
+				validNumber = double.TryParse(input, out radius);
+			}
+			if (radius < 0)
+			{
+				Console.WriteLine("You entered a negative number, but I'll compute the radius anyway.");
+			}
+			// Computes the area of a circle and prints out the result.
+			double area = Math.PI * Math.Pow(radius, 2);
+			Console.WriteLine($"The area of the circle is {area:N2}");
+			Console.WriteLine("Now enter a height the radius. =>");
+			input = Console.ReadLine();
+			double height;
+			validNumber = double.TryParse(input, out height);
+			while (!validNumber)
+			{
+				Console.WriteLine("Invalid input. Please enter a number. =>");
+				input = Console.ReadLine();
+				validNumber = double.TryParse(input, out height);
+			}
+			area = Math.PI * Math.Pow(radius, 2) * height;
+			Console.WriteLine($"The volume of the circle is {area:N2}");
+		}
+	}
+}
---
+++ Loop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Loop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//for loop
+			Console.WriteLine("The loop is about to start");
+			for (int x = 3; x < 30; x += 7)
+			{
+				Console.WriteLine($"The loop is running {x}");
+			}
+			Console.WriteLine("The loop has ended");
+
+			//while loop
+			bool keepGoing = true;
+			while (keepGoing)
+			{
+				Console.WriteLine("Looped");
+				Console.WriteLine("Continue? (y/n):");
+				string goOn = Console.ReadLine();
+				if (goOn == "n" || goOn == "N")
+				{
+					keepGoing = false;
+				}
+				else if (goOn != "y" && goOn != "Y")
+				{
+					Console.WriteLine("That's not valid input.");
+					Console.WriteLine("Defaulting to no.");
+					keepGoing = false;
+				}
+			}
+
+			//do while loop
+			int count = 0;
+			do
+			{
+				Console.WriteLine(count);
+				count++;
+			} while (count < 10);
+			Console.WriteLine("All done.");
+			Console.WriteLine(count);
+
+			//foreach loop
+			int[] sizes = { 156, 456, 786, 468, 763, 896 };
+			foreach (int size in sizes)
+			{
+				Console.WriteLine(size);
+			}
+
+			//loop exercise
+			double[] sizes2 = { 10.5, 20.12, 100.543, 1234.12345, 50.24542 };
+			foreach (double size in sizes2)
+			{
+				Console.WriteLine($"The current number is {size:N2}.");
+			}
+		}
+	}
+}
