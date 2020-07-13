f7657e576f212d655d2e761b93d5f7d5036d04e4
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-13 11:32:19 -0400

    Added string examples.

---
+++ Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string word1 = "Test";
+			string word2 = "TEST";
+
+			if (String.Equals(word1, word2, StringComparison.CurrentCultureIgnoreCase))
+			{
+				Console.WriteLine($"{word1} and {word2} are the same.");
+			}
+
+			if (String.Compare(word1, word2, true) == 0)
+			{
+				Console.WriteLine($"{word1} and {word2} are the same.");
+			}
+
+			Console.WriteLine(word1.Equals(word2));
+
+			string word3 = "Grand Circus";
+			Console.WriteLine(word3.Substring(6, 3));
+
+			StringBuilder builder = new StringBuilder();
+			builder.Append("Hello ");
+			builder.Append("World!");
+			builder.Append("\nHow's it going?");
+			builder.Insert(0, "\n");
+			builder.Replace('o', 'X');
+			builder.Replace(" ", ",_");
+			string newString = builder.ToString();
+			Console.WriteLine(newString);
+		}
+	}
+}
