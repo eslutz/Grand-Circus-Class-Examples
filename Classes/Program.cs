a9c151181b3a6bcb3c61b2782d1d3948742f6ff9
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-22 12:00:22 -0400

    Added class example.

---
+++ Classes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Classes
+{
+	class Spaceship
+	{
+		private string Name;
+		private string ShipType;
+		private bool FTLCapable;
+		private double CrewCapacity;
+
+		public Spaceship(string name, string type, bool ftlCapable, double crew)
+		{
+			Name = name;
+			ShipType = type;
+			FTLCapable = ftlCapable;
+			CrewCapacity = crew;
+		}
+
+		public double Travel()
+		{
+			double distanceTraveled = 10000;
+			return distanceTraveled;
+		}
+
+		public string GetShip()
+		{
+			return ShipType + " " + Name;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Spaceship ship = new Spaceship("Galactica", "Battlestar", true, 5000);
+			
+			Console.WriteLine(ship.GetShip());
+			Console.WriteLine($"The ship moved: {ship.Travel()}");
+		}
+	}
+}
