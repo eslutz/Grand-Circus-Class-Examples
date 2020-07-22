f4520740ec9dec9a3b3b55fd6be65d5855b974ba
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-22 14:37:01 -0400

    Added more class examples.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,19 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Classes
 {
 	class Spaceship
 	{
-		private string Name;
-		private string ShipType;
-		private bool FTLCapable;
-		private double CrewCapacity;
+		private string name;
+		private string shipType;
+		private bool _FTLCapable;
+		private double crewCapacity;
 
+		public string Name
+		{
+			get { return name; }
+		}
+		public string ShipType { get; set; }
+		public bool FTLCapable { get { return _FTLCapable; } set { _FTLCapable = value; } }
+		public double CrewCapacity
+		{
+			get { return crewCapacity; }
+			set { crewCapacity = value; }
+		}
+		
+
+		public Spaceship()
+		{
+			name = "unnamed";
+			shipType = "unknown";
+			_FTLCapable = false;
+			CrewCapacity = 0;
+		}
+		
 		public Spaceship(string name, string type, bool ftlCapable, double crew)
 		{
-			Name = name;
-			ShipType = type;
-			FTLCapable = ftlCapable;
+			this.name = name;
+			shipType = type;
+			_FTLCapable = ftlCapable;
 			CrewCapacity = crew;
 		}
 
@@ -33,9 +55,37 @@ namespace Classes
 		static void Main(string[] args)
 		{
 			Spaceship ship = new Spaceship("Galactica", "Battlestar", true, 5000);
-			
+
 			Console.WriteLine(ship.GetShip());
 			Console.WriteLine($"The ship moved: {ship.Travel()}");
+
+			Spaceship[] fleet = new Spaceship[10];
+			for (int x = 0; x < fleet.Length; x++)
+			{
+				fleet[x] = new Spaceship();
+			}
+			foreach (Spaceship vessle in fleet)
+			{
+				Console.WriteLine(vessle.GetShip());
+			}
+
+			List<Spaceship> fleet2 = new List<Spaceship>();
+			fleet2.Add(new Spaceship());
+			fleet2.Add(new Spaceship("Galactica", "Battlestar", true, 5000));
+			fleet2.Add(new Spaceship("Serenity ", "Firefly", false, 5));
+			fleet2.Add(new Spaceship());
+			fleet2.Add(new Spaceship("Rocinante ", "Gunship", false, 20));
+			foreach (Spaceship vessle in fleet2)
+			{
+				Console.WriteLine(vessle.GetShip());
+			}
+
+			ship.CrewCapacity = 200;
+			Console.WriteLine(ship.CrewCapacity);
+
+			Console.WriteLine(ship.FTLCapable);
+						ship.FTLCapable = true;
+			Console.WriteLine(ship.FTLCapable);
 		}
 	}
 }
