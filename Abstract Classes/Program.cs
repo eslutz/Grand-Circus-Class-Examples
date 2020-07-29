eaffef4573f07297caf734aea1c870e1d0d80842
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-29 12:01:52 -0400

    Added abstract class and enum examples.

---
+++ Abstract Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Abstract_Classes
+{
+	abstract class Car
+	{
+		public abstract void Drive();
+	}
+
+	class Chevy : Car
+	{
+		public override void Drive()
+		{
+			Console.WriteLine("I'm a Chevy! And I'm driving!");
+		}
+	}
+	sealed class Ford : Car
+	{
+		public override void Drive()
+		{
+			Console.WriteLine("I'm a Ford! And I'm driving!");
+		}
+	}
+	
+	//Cannot inherit Ford because it is sealed.
+	//class Testcar : Ford { }
+	 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Chevy corvette = new Chevy();
+			Ford focus = new Ford();
+			TestTrack(corvette);
+			TestTrack(focus);
+		}
+	
+		static void TestTrack(Car thecar)
+		{
+			thecar.Drive();
+		}
+	}
+}
---
+++ Enumerations/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enumerations
+{
+	enum Suit
+	{
+		Clubs,
+		Diamonds,
+		Hearts,
+		Spades
+	}
+
+	enum Rank
+	{
+		Two = 2,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+		Ace
+	}
+	
+	class Card
+	{
+		public Suit suit { get; set; }
+		public Rank rank { get; set; }
+	}
+
+	class Deck
+	{
+		public List<Card> Cards = new List<Card>();
+
+		public Deck()
+		{
+			Card first = new Card() { rank = Rank.Two, suit = Suit.Clubs };
+			Cards.Add(first);
+			Card second = new Card() { rank = Rank.Ace, suit = Suit.Spades };
+			Cards.Add(second);
+		}
+
+		public void Play()
+		{
+			if(Cards[0].rank < Cards[1].rank)
+			{
+				Console.WriteLine("The first is less than the second.");
+			}
+			else
+			{
+				Console.WriteLine("The first is greater than or equal to the second.");
+
+			}
+		}
+	}
+
+	enum GuitarStyle
+	{
+		acoustic,
+		electric
+	}
+
+	class Guitar
+	{
+		public GuitarStyle Style { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Guitar fender = new Guitar();
+			fender.Style = GuitarStyle.electric;
+
+			Guitar gibson = new Guitar();
+			gibson.Style = GuitarStyle.acoustic;
+
+			Console.WriteLine(fender.Style);
+			Console.WriteLine(gibson.Style);
+
+			Deck myDeck = new Deck();
+			myDeck.Play();
+		}
+	}
+}
