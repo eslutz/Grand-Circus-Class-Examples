7f05f0d3483f4c22e32feea6d9c42ae044944e0f
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-07-29 14:25:05 -0400

    Added interface example.

--- Enumerations/Program.cs
+++ Enumerations/Program.cs
@@ -30,8 +30,29 @@ namespace Enumerations
 	
 	class Card
 	{
+		private static Random rand = new Random();
 		public Suit suit { get; set; }
 		public Rank rank { get; set; }
+
+		public override string ToString()
+		{
+			int ordinal = (int)rank;
+			if(ordinal <= 10)
+			{
+				return $"{ordinal} of {suit}";
+			}
+			else
+			{
+				return $"{rank} of {suit}";
+			}
+		}
+
+		public static Card RandomCard()
+		{
+			int suitNum = rand.Next(Enum.GetValues(typeof(Suit)).Length);
+			int rankNum = rand.Next(2, Enum.GetValues(typeof(Rank)).Length + 2);
+			return new Card() { rank = (Rank)rankNum, suit = (Suit)suitNum };
+		}
 	}
 
 	class Deck
@@ -40,10 +61,39 @@ namespace Enumerations
 
 		public Deck()
 		{
-			Card first = new Card() { rank = Rank.Two, suit = Suit.Clubs };
-			Cards.Add(first);
-			Card second = new Card() { rank = Rank.Ace, suit = Suit.Spades };
-			Cards.Add(second);
+			
+		}
+
+		public void Add(Card card)
+		{
+			Cards.Add(card);
+		}
+
+		public void FillDeck()
+		{
+			Cards.Clear();
+			foreach(Suit s in Enum.GetValues(typeof(Suit)))
+			{
+				foreach (Rank r in Enum.GetValues(typeof(Rank)))
+				{
+					Cards.Add(new Card() { rank = r, suit = s });
+				}
+			}
+			//for (int s = 0; s <= 3; s++)
+			//{
+			//	for (int r = 2; r <= 14; r++)
+			//	{
+			//		Cards.Add(new Card() { rank = (Rank)r, suit = (Suit)s});
+			//	}
+			//}
+		}
+
+		public void ListDeck()
+		{
+			foreach(Card card in Cards)
+			{
+				Console.WriteLine(card);
+			}
 		}
 
 		public void Play()
@@ -55,7 +105,6 @@ namespace Enumerations
 			else
 			{
 				Console.WriteLine("The first is greater than or equal to the second.");
-
 			}
 		}
 	}
@@ -81,11 +130,27 @@ namespace Enumerations
 			Guitar gibson = new Guitar();
 			gibson.Style = GuitarStyle.acoustic;
 
-			Console.WriteLine(fender.Style);
-			Console.WriteLine(gibson.Style);
+			//Console.WriteLine(fender.Style);
+			//Console.WriteLine(gibson.Style);
 
-			Deck myDeck = new Deck();
-			myDeck.Play();
+			Deck mydeck = new Deck();
+			//Card first = new Card() { rank = Rank.Jack, suit = Suit.Diamonds };
+			//Card second = new Card() { rank = Rank.Ten, suit = Suit.Diamonds };
+			//mydeck.Add(first);
+			//mydeck.Add(second);
+			mydeck.FillDeck();
+			mydeck.ListDeck();
+
+			Console.WriteLine();
+			for (int x = 0; x < 100; x++)
+			{
+				Console.WriteLine(Card.RandomCard());
+			}
+			Console.WriteLine();
+
+			int size = Enum.GetValues(typeof(Suit)).Length;
+			Console.WriteLine($"Suit size: {size}");
+			Console.WriteLine($"Rank size: {Enum.GetValues(typeof(Rank)).Length}");
 		}
 	}
 }
---
+++ Interfaces/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Interfaces
+{
+    interface ICar
+    {
+        public void Drive();
+    }
+
+    interface ITurbo
+	{
+        public void ActivateTurbo();
+	}
+
+    class Chevy : ICar, ITurbo
+    {
+        public void Drive()
+        {
+            Console.WriteLine("I'm a Chevy! And I'm driving!");
+        }
+
+        public void ActivateTurbo()
+        {
+            Console.WriteLine("Turbo has been activated on your Chevy!");
+        }
+    }
+
+    class Ford : ICar, ITurbo
+    {
+        public void Drive()
+        {
+            Console.WriteLine("I'm a Ford! And I'm driving!");
+        }
+
+        public void ActivateTurbo()
+		{
+			Console.WriteLine("Turbo has been activated on your Ford!");
+		}
+    }
+
+    class Kia : ICar
+	{
+        public void Drive()
+		{
+            Console.WriteLine("I'm a Kia! And I'm driving!");
+        }
+    }
+
+    class Program
+    {
+        static void TestTrack(ICar thecar)
+        {
+            thecar.Drive();
+        }
+
+        static void TestTurbo(ITurbo theturbo)
+		{
+            theturbo.ActivateTurbo();
+		}
+        static void Main(string[] args)
+        {
+            Chevy trax = new Chevy();
+            Ford escort = new Ford();
+            Kia grapevan = new Kia();
+            TestTrack(trax);
+            TestTrack(escort);
+            TestTrack(grapevan);
+            TestTurbo(trax);
+            TestTurbo(escort);
+            //Not possible because Kia doesn't implement Turbo.
+            //TestTurbo(grapevan);
+        }
+    }
+}
