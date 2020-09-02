e80bec93f48ae9b38701a21930f55138e0e2e9ee
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-02 11:35:39 -0400

    Added second web app example.

---
+++ SecondWebApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SecondWebApp.Models;
+
+namespace SecondWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult Demo()
+		{
+			List<string> myList = new List<string>();
+			myList.Add("Hello");
+			myList.Add("There");
+			myList.Add("World");
+			myList.Add("Have a great day");
+
+			ViewBag.List = myList;
+			return View();
+		}
+	}
+}
---
+++ SecondWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecondWebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
