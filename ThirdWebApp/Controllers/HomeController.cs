6290c58d95f6990fea7be23e0022befd731b1992
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-02 13:57:02 -0400

    Added model example.

--- ThirdWebApp/Controllers/HomeController.cs
+++ ThirdWebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThirdWebApp.Models;
 
 namespace ThirdWebApp.Controllers
 {
@@ -22,6 +23,8 @@ namespace ThirdWebApp.Controllers
 		[HttpPost]
 		public ActionResult Save(string username, string email, string password)
 		{
+			WebUser user = new WebUser() { Username = username, Email = email, Password = password };
+
 			return View();
 		}
 	}
