81ce4cfb98d08e9b50ac7e291598302ddb3c7aed
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-02 13:45:27 -0400

    Made some updates.

--- ThirdWebApp/Controllers/HomeController.cs
+++ ThirdWebApp/Controllers/HomeController.cs
@@ -13,21 +13,14 @@ namespace ThirdWebApp.Controllers
 			return View();
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
-
+		[HttpGet]
 		public ActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Save(string username, string email, string password)
 		{
 			return View();
 		}
