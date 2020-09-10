12b60867e60407c645be86fde2c953abd1084fbe
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-10 14:27:46 -0400

    Added database connection example.

---
+++ DapperConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+namespace DapperConsole
+{
+	class Categories
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public string Description { get; set; }
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//IDbConnection database = new SqlConnection("Server=BCKW433\\SQLEXPRESS;Database=Northwind;user id=TestUser;password=password");
+			IDbConnection database = new SqlConnection("Server=BCKW433\\SQLEXPRESS;Database=Northwind;Trusted_Connection=Yes");
+			database.Open();
+
+			List<Categories> cats = database.Query<Categories>("select * from Categories").AsList<Categories>();
+
+			foreach(Categories cat in cats)
+			{
+				Console.WriteLine($"{cat.CategoryID} {cat.CategoryName} {cat.Description}");
+			}
+
+			database.Close();
+		}
+	}
+}
