545641d08562f4f50456266e3f63e18f9387aa79
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-14 13:24:24 -0400

    Added Pet Store example.

---
+++ PetStore/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace PetStore.Models
+{
+	public class Product
+	{
+		[Key]
+		public long ID { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+
+		//Create
+
+		//Read
+
+		//Update
+
+		//Delete
+	}
+}
