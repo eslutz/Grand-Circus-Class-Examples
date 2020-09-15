b2722a6cd0dfb728738c538d7e53f1961b2e41a3
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-15 10:14:06 -0400

    Added Wonderland example.

---
+++ Wonderland/Models/Blog1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace Wonderland.Models
+{
+	public class Blog1
+	{
+		public long id { get; set; }
+		public string Paragraphs { get; set; }
+		public string Title { get; set; }
+	}
+}
