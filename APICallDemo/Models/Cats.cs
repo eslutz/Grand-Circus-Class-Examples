ebaf6e671604ac67902d1bc19bfd0a3bfac74af9
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-21 20:05:26 -0400

    Finished API examples.

---
+++ APICallDemo/Models/Cats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICallDemo.Models
+{
+	public class Cats
+	{
+		public string Fact { get; set; }
+		public int Length { get; set; }
+		public CatDetail[] more { get; set; }
+	}
+}
--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TestAPI
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			await test();
+
+			//HttpClient client = new HttpClient();
+			//client.BaseAddress = new Uri("https://catfact.ninja");
+			//string response = client.GetAsync("fact").Result.Content.ReadAsStringAsync().Result;
+			//Console.WriteLine(response);
+		}
+
+		public static async Task test()
+		{
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri("https://catfact.ninja");
+			var response = await client.GetAsync("/fact");
+			var catstring = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(catstring);
 		}
 	}
 }
