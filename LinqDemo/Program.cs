5dc767400a642a0ecad3c199db8d7934ec6765c4
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-09-23 14:51:05 -0400

    Completed Linq demo.

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -18,13 +18,15 @@ namespace LinqDemo
 			items.Add(new Info() { ID = 1, Name = "Dwight" });
 			items.Add(new Info() { ID = 2, Name = "Mose" });
 			items.Add(new Info() { ID = 3, Name = "Jim" });
-			items.Add(new Info() { ID = 4, Name = "Pam" });
+			items.Add(new Info() { ID = 7, Name = "Pam" });
 			items.Add(new Info() { ID = 5, Name = "Ryan" });
 			items.Add(new Info() { ID = 6, Name = "Creed" });
 
+			//List<Info> results = items.Where(x => x.ID > 2).ToList();
+			//var results = items.Where(x => x.ID > 2).OrderBy(x => x.ID);
+			var results = (from item in items where item.ID > 2 select item).OrderBy(x => x.ID);
 
-
-			foreach(Info item in items)
+			foreach(Info item in results)
 			{
 				Console.WriteLine(item.Name);
 			}
