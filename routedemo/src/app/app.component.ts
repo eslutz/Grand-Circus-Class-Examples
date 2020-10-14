266128fd3b90f9b29433350a9dfcfd5afa9ecc47
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-10-14 14:51:52 -0400

    Completed routing demo.

--- routedemo/src/app/app.component.ts
+++ routedemo/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routedemo';
+  myParams = { test: "abc123", orderNumber: "order1000" };
 }
--- routedemo/src/app/second/second.component.ts
+++ routedemo/src/app/second/second.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  testClick() {
+      console.log(this.route.queryParams);
+  }
 }
